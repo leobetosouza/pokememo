04fb678cc1679a5c04319b361ee88ee57036e207
Leonardo Alberto Souza <leobetosouza@gmail.com>
2020-04-09 17:25:52 -0300

    imprementing offline first cache

--- src/requester.js
+++ src/requester.js
@@ -1,10 +1,65 @@
+import localforage from "localforage";
 import { getRadomItem } from "./utils";
 
 const BASE_URL = "https://pokeapi.co/api/v2";
 
-const ajax = async url => {
+const apicache = localforage.createInstance({
+  driver: localforage.INDEXEDDB,
+  name: "pokeapi-cache",
+  version: 1.0,
+  storeName: "apicache",
+  description: "Cache of PokeAPI responses",
+});
+
+const imgcache = localforage.createInstance({
+  driver: localforage.INDEXEDDB,
+  name: "pokeapi-images-cache",
+  version: 1.0,
+  storeName: "imgcache",
+  description: "Cache of PokeAPI images",
+});
+
+const ajax = async (url) => {
+  await apicache.ready();
+
+  const cached = await apicache.getItem(url);
+  if (cached) {
+    console.log(`from cache ${url}`, cached);
+    return cached;
+  }
+
   console.log(`getting: ${url}`);
-  return fetch(url).then(res => res.json());
+  const data = await fetch(url).then((res) => res.json());
+
+  await apicache.setItem(url, { ...data, cached: true });
+  return data;
+};
+
+const arrayBufferToBase64 = (buffer) => {
+  const bytes = [...new Uint8Array(buffer)].reduce(
+    (acc, b) => acc + String.fromCharCode(b),
+    ""
+  );
+
+  return "data:image/jpeg;base64," + btoa(bytes);
+};
+
+const fetchImage = async (url) => {
+  await imgcache.ready();
+
+  const cached = await imgcache.getItem(url);
+  if (cached) {
+    console.log(`img from cache ${url}`, cached);
+    return cached.img;
+  }
+
+  console.log(`img getting: ${url}`);
+  const img = await fetch(url)
+    .then((res) => res.arrayBuffer())
+    .then(arrayBufferToBase64);
+
+  await imgcache.setItem(url, { img, cached: true });
+  return img;
 };
 
 const getPkmnUrl = ({ varieties }) => getRadomItem(varieties).pokemon.url;
@@ -16,7 +71,10 @@ const getLanguageObject = ({ names }) =>
   Object.values(names).find(languageFilter);
 
 const getName = (form, specie) =>
-  (getLanguageObject(form) || getLanguageObject(specie)).name;
+  (getLanguageObject(form) || getLanguageObject(specie)).name
+    .split(" ")
+    .filter((w) => !["Mega", "Alolan", "Totem"].includes(w))
+    .join(" ");
 
 const getFlavorText = ({ flavor_text_entries, genera }) => {
   const genus = genera.find(languageFilter).genus;
@@ -25,20 +83,6 @@ const getFlavorText = ({ flavor_text_entries, genera }) => {
 
   return `${genus}: ${flavor_text}`;
 };
-
-const arrayBufferToBase64 = buffer => {
-  const bytes = [...new Uint8Array(buffer)].reduce(
-    (acc, b) => acc + String.fromCharCode(b),
-    ""
-  );
-
-  return "data:image/jpeg;base64," + btoa(bytes);
-};
-
-const fetchImage = async url =>
-  await fetch(url)
-    .then(res => res.arrayBuffer())
-    .then(arrayBufferToBase64);
 
 const getImgs = async (form, pkmn) => {
   const default_sprite_url =
@@ -51,7 +95,7 @@ const getImgs = async (form, pkmn) => {
   if (shiny_sprite_url) {
     const images = await Promise.all([
       fetchImage(shiny_sprite_url),
-      fetchImage(default_sprite_url)
+      fetchImage(default_sprite_url),
     ]);
 
     return { shiny: images[0], common: images[1] };
@@ -69,26 +113,35 @@ const fetchTypes = async () => {
   return res.reduce(
     (acc, { name, names }) => ({
       ...acc,
-      [name]: names.find(languageFilter).name
+      [name]: names.find(languageFilter).name,
     }),
     {}
   );
 };
 
-const getType = (types, pkmn) =>
+const getTypes = (types, pkmn) =>
   pkmn.types
     .sort((a, b) => a.slot - b.slot)
-    .map(({ type }) => types[type.name])
-    .join("/");
+    .map(({ type }) => types[type.name]);
 
-const getNationalNumber = ({ pokedex_numbers }) =>
-  pokedex_numbers.find(({ pokedex }) => pokedex.name === "national")
-    .entry_number;
+const getNationalNumber = (specie, n) => {
+  const { pokedex_numbers } = specie;
+  try {
+    return pokedex_numbers.find(({ pokedex }) => pokedex.name === "national")
+      .entry_number;
+  } catch (e) {
+    console.error(e, specie);
+    return n;
+  }
+};
 
-const requester = async numbers => {
+const isUltraBeast = ({ abilities }) =>
+  abilities.some(({ ability }) => ability.name === "beast-boost");
+
+const requester = async (numbers) => {
   const types = await fetchTypes();
 
-  const promises = numbers.map(async n => {
+  const promises = numbers.map(async (n) => {
     const specie = await ajax(`${BASE_URL}/pokemon-species/${n}/`);
     let pkmn,
       form,
@@ -105,12 +158,17 @@ const requester = async numbers => {
     if (!imgs.common) throw new Error(`Fail to get pkmn#${n} image`);
 
     return {
-      id: getNationalNumber(specie),
+      id: getNationalNumber(specie, n),
       color: specie.color.name,
       name: getName(form, specie),
       imgs,
       flavor_text: getFlavorText(specie),
-      type: getType(types, pkmn)
+      types: getTypes(types, pkmn),
+      is_mega: form.is_mega,
+      is_baby: specie.is_baby,
+      is_ultra_beast: isUltraBeast(pkmn),
+      is_alolan_form: form.form_name.includes("alola"),
+      is_totem: form.form_name.includes("totem"),
     };
   });
 
